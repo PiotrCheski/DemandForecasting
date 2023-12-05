6283c80ee309b7c784282775baa968606abdf582
Piotr98765 <61555492+PiotrCheski@users.noreply.github.com>
2023-12-05 23:50:24 +0100

    New charts

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import os
 import pandas as pd
 from flask import Flask, render_template, redirect, url_for, request
 from werkzeug.utils import secure_filename
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import numpy as np
 import io, base64
 
 app = Flask(__name__)
@@ -78,7 +81,29 @@ def see_specific_file(file_name):
     smoothed_values_015 = calculate_exp_smoothing(data, 0.15)
     smoothed_values_020 = calculate_exp_smoothing(data, 0.20)
 
-    chart = generate_chart(data)
+    # Wykres dla oryginalnego zestawu danych
+    chart_data = generate_chart(data)
+
+    # Wykres dla prognozy metodą średniej stałej
+    data_average = data.copy()
+    next_month_index = data_average.index.max() + 1
+    data_average.loc[next_month_index, 'Okres'] = data_average['Okres'].max() + 1
+    data_average.loc[next_month_index, 'Wartosci'] = average
+    chart_average = generate_chart(data_average, isAverage=True)
+
+    # Wykres dla prognozy metodą średniej ruchomej
+    data_moving = data.copy()
+    next_month_index = data_moving.index.max() + 1
+    data_moving.loc[next_month_index, 'Okres'] = data_moving['Okres'].max() + 1
+    data_moving["n3"] = data_moving["Wartosci"]
+    data_moving["n6"] = data_moving["Wartosci"]
+    data_moving["n9"] = data_moving["Wartosci"]
+    
+    data_moving.loc[next_month_index, 'n3'] = moving_avg_n3[-1]
+    data_moving.loc[next_month_index, 'n6'] = moving_avg_n6[-1]
+    data_moving.loc[next_month_index, 'n9'] = moving_avg_n9[-1]
+    print(data_moving)
+    chart_moving = generate_chart(data_moving, isAverageMoving=True)
 
     return render_template("specificFile.html", 
                            file_name=file_name, 
@@ -90,7 +115,10 @@ def see_specific_file(file_name):
                            smoothed_values_010=smoothed_values_010,
                            smoothed_values_015=smoothed_values_015,
                            smoothed_values_020=smoothed_values_020, 
-                           chart=chart)
+                           chart_data=chart_data,
+                           chart_average=chart_average,
+                           chart_moving=chart_moving
+                           )
 
 
 @app.route('/delete_file/<file_name>', methods=['GET', 'POST'])
@@ -132,17 +160,38 @@ def params_linear_regression(data):
     sum_y2 = sum(data["Wartosci"]**2)
     return n, sum_x, sum_y, sum_x2, sum_y2
 
-def generate_chart(data):
-    plt.plot(data['Okres'], data['Wartosci'], marker='o', linestyle='None', markersize=1, color='blue')
-    # Dodaj etykiety osi
-    plt.xlabel('Okress]', fontsize=14)
+def generate_chart(data, isAverage=None, isAverageMoving=None):
+    fig, ax = plt.subplots()
+
+    # Set the background color for the figure and axis
+    fig.set_facecolor('#5c78a3')
+    ax.set_facecolor('#5c78a3')
+
+    # Create a bar chart
+    plt.plot(data['Okres'], data['Wartosci'], marker='o', linestyle='-', markersize=5, color='#00cc00')
+    if isAverage==True:
+            # Color the last point in yellow
+            plt.scatter(data['Okres'].iloc[-1], data['Wartosci'].iloc[-1], color='yellow', s=50, zorder=10)
+    # Add labels to the axes
+    plt.xlabel('Okres', fontsize=14)
     plt.ylabel('Wartość', fontsize=14)
+
+    # Set x-axis ticks to display all values
+    plt.xticks(data['Okres'])  # Adjust rotation and ha as needed
+    max_value = max(data['Wartosci'])
+    y_ticks = np.arange(0, 1.1*max_value, 50)
+    plt.yticks(y_ticks)
+    plt.grid(True, linestyle='--', alpha=0.7)
+    # Save the figure to a BytesIO object
     obraz = io.BytesIO()
-    plt.savefig(obraz, format='png')   
+    plt.savefig(obraz, format='png')
     obraz.seek(0)
-    obraz64 = obraz.read()
-    obraz64 = base64.b64encode(obraz64)
-    obraz64 = obraz64.decode()
+    # Convert the image to base64
+    obraz64 = base64.b64encode(obraz.read()).decode()
+
+    # Close the figure to free up resources
+    plt.close(fig)
+
     return obraz64
 
 if __name__ == "__main__":
